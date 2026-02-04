1988c1e532da8d7459dbbbc2902bf4708716ab06
JessMarie05 <Chamberlain.jessm@gmail.com>
2026-02-04 18:07:39 +0000

    change 49 to 52

--- src/project20280/exercises/Wk1.java
+++ src/project20280/exercises/Wk1.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Wk1 {
 
     public static void q1() {
-        int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 52};
 
         System.out.println(my_array);
 
